d5cfb984a85d1d12950ec033e2d66034cb9ccd89
buyath <buyaththayub@gmail.com>
2026-04-16 17:47:59 +0000

    feat :file manager project completed

---
+++ file-manager-project/app.js
@@ -0,0 +1,51 @@
+/*
+🔥 Mini Project 1: File Manager CLI
+
+Commands:
+
+node app.js create test.txt
+node app.js read test.txt
+node app.js delete test.txt
+node app.js rename old.txt new.txt
+
+👉 This will make you strong fast.
+*/
+const fs =  require('fs');
+const path = require('path');
+// const { comma } = require('postcss/lib/list');
+const command = process.argv[2];
+const fileName = process.argv[3];
+const newFileName = process.argv[4];
+const filePath = path.join(__dirname , "files",fileName);
+
+if(command == "create"){
+    fs.writeFileSync(filePath,"This file  was created using Node js");
+}
+
+else if (command == "read"){
+    const data = fs.readFileSync(filePath,{encoding:'utf-8'});
+    console.log(data);
+
+}
+else if (command == "delete"){
+    try{
+        fs.unlinkSync(filePath);
+        console.log("File deleted SuccessFully");   
+    }
+    catch(err){
+        console.log("Error deleting File",err.message);
+    }
+}
+else if(command == "rename"){
+    const newFilePath  = path.join(__dirname , "files", newFileName);
+    fs.renameSync(filePath,newFilePath,(err) => {
+        if(err){
+            throw err;
+            return;
+        }
+    })
+
+}
+else{
+    console.log("invalid command");
+}
