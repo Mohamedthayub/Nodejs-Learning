fafcfa33841c4401c8f74ce52786389c40d72ed1
buyath <buyaththayub@gmail.com>
2026-04-17 17:52:08 +0000

    completed:hard exercises in nodejs

---
+++ exercises/hard/fs.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const { report } = require('process');
+
+
+const filePath = path.join("/data","users","profile.json");
+console.log(filePath);
+
+const filePath2 = path.resolve("data","users","profile.json");
+console.log(filePath2);
+
+/*
+the diffrence between path.join() and path.resolve 
+
+path.join()
+
+Combines path segments into a normalized path.
+
+path.resolve()
+
+Builds an absolute path starting from current working directory (unless already absolute).
+*/
+
+/*
+🧠 Exercise 10: File Extension Checker
+🎯 Goal
+
+Learn how to extract file types from file names.
+
+Examples:
+
+resume.pdf
+photo.png
+notes.txt
+
+You need to print:
+
+.pdf
+.png
+.txt
+*/
+
+const exts = ["resume.pdf", "photo.png", "notes.txt","thayub.exe"];
+
+for(let i = 0; i<exts.length; i++){
+    const extension = path.extname(exts[i]);
+    if(extension != '.exe'){
+        console.log(exts[i] + " -> " + extension);
+    
+    }
+}
+// chatgpt task 1
+/*
+1. Build path: documents/work/report.pdf
+2. Get extension from report.pdf
+3. Print full absolute path of that file
+*/
+let buildPath = "documents/work/report.pdf"
+console.log(path.resolve(buildPath)); // here i join path using resolve function ;
+console.log(path.join(__dirname ,"documents","work","report.pdf")); // here i join path using join function 
+console.log(path.extname("report.pdf")); // it extname function gets  the extension of the file name ;
+
+
